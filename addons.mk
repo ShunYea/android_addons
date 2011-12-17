01a0b0191b7190a56bcee85872edcfbdf90148d5
ShunYea <shunyea@msn.com>
2011-12-17 18:31:19 +0800

    add gps.config

--- addons.mk
+++ addons.mk
@@ -1,3 +1,4 @@
-# the necessary apk
+# the necessary file
 PRODUCT_COPY_FILES += \
     addons/app/RootExplorer.apk:system/app/RootExplorer.apk \
+    addons/etc/gps.conf_CN_SUPL:system/etc/gps.conf
